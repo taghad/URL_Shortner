76fbe93f07c7e5c226ee47d2991aa81016973d21
mohamadHasan <mhtaghad99@gmail.com>
2019-12-18 13:11:52 +0330

    add URL model

--- model/url.go
+++ model/url.go
@@ -1,2 +1,10 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
+type URL struct {
+	gorm.Model
+	URL   string `gorm:"unique_index;not null"`
+	User User `gorm:"association_foreignkey:Username"`
+}
+
