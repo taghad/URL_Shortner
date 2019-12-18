e87af1b03cb88e0f863b99882983877dcc614420
mohamadHasan <mhtaghad99@gmail.com>
2019-12-18 13:06:54 +0330

    add user model

--- model/user.go
+++ model/user.go
@@ -1,1 +1,27 @@
 package model
+
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	Username   string `gorm:"unique_index;not null"`
+	Email      string `gorm:"unique_index;not null"`
+	Password   string `gorm:"not null"`
+}
+
+func (u *User) HashPassword(plain string) (string, error) {
+	if len(plain) == 0 {
+		return "", errors.New("password should not be empty")
+	}
+	h, err := bcrypt.GenerateFromPassword([]byte(plain), bcrypt.DefaultCost)
+	return string(h), err
+}
+
+func (u *User) CheckPassword(plain string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plain))
+	return err == nil
+}
