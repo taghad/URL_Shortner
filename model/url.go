994808bd8c17fd729f70c3b2fc65ef0905561438
mohamadHasan <mhtaghad99@gmail.com>
2019-12-19 01:42:48 +0330

    add UrlStore method

--- model/url.go
+++ model/url.go
@@ -4,7 +4,8 @@ import "github.com/jinzhu/gorm"
 
 type URL struct {
 	gorm.Model
-	URL   string `gorm:"unique_index;not null"`
 	User User `gorm:"association_foreignkey:Username"`
+	URL   string `gorm:"unique_index;not null"`
+	ShortURL string `gorm:"unique_index;not null"`
 }
 
--- store/url.go
+++ store/url.go
@@ -21,3 +21,14 @@ func (u *UrlStore) Create(url *model.URL) (err error) {
 func (u *UrlStore) Delete(url * model.URL) (err error)  {
 	return u.db.Delete(u).Error
 }
+
+func (u *UrlStore) GetByShortURL(shortURL string) (*model.URL, error) {
+	var m model.URL
+	if err := u.db.Where(&model.URL{ShortURL: shortURL}).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &m, nil
+}
