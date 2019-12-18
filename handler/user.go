5565a05f111b12027fd6df651d8fe914a6dec306
mohamadHasan <mhtaghad99@gmail.com>
2019-12-18 13:58:40 +0330

    add hadler file

---
+++ handler/user.go
@@ -0,0 +1,15 @@
+package handler
+
+import "net/http"
+
+func (h *Handler) SignUp(c echo.Context) error {
+	var u model.User
+	req := &userRegisterRequest{}
+	if err := req.bind(c, &u); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, utils.NewError(err))
+	}
+	if err := h.userStore.Create(&u); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, utils.NewError(err))
+	}
+	return c.JSON(http.StatusCreated, newUserResponse(&u))
+}
